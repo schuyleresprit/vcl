bc05af16d1d7b98d46582c777e23a5d7b9c98052
schuyleresprit <schuyleresprit@gmail.com>
2023-02-13 15:55:59 -0500

    major data update

--- convert2.py
+++ convert2.py
@@ -8,7 +8,7 @@ def convjson(csvFilename, jsonFilename):
     mydata = {}
 
     # reading the data from CSV file
-    with open(csvFilename, encoding = 'utf-8') as csvfile:
+    with open(csvFilename) as csvfile:
         csvRead = csv.DictReader(csvfile)
 
         # Converting rows into dictionary and adding it to data
@@ -18,8 +18,8 @@ def convjson(csvFilename, jsonFilename):
             mydata[mykey] = rows
 
     # dumping the data
-    with open(jsonFilename, 'w', encoding = 'utf-8') as jsonfile:
-        jsonfile.write(json.dumps(mydata, indent = 4))
+    with open(jsonFilename, 'w') as jsonfile:
+        jsonfile.write(json.dumps(mydata, ensure_ascii=False, indent = 4))
 
 # filenames
 csvFilename = r'vcl_author_info.csv'
