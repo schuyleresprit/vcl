b623eb0899d6c1008f415cc8944e3018d399f1ed
schuyleresprit <schuyleresprit@gmail.com>
2024-07-12 17:22:24 -0500

    add bios to top of authors page

---
+++ biosconvert.py
@@ -0,0 +1,29 @@
+import csv
+import json
+
+# defining the function to convert CSV file to JSON file
+def convjson(csvFilename, jsonFilename):
+
+    # creating a dictionary
+    mydata = {}
+
+    # reading the data from CSV file
+    with open(csvFilename) as csvfile:
+        csvRead = csv.DictReader(csvfile)
+
+        # Converting rows into dictionary and adding it to data
+        for rows in csvRead:
+
+            mykey = rows['authorid']
+            mydata[mykey] = rows
+
+    # dumping the data
+    with open(jsonFilename, 'w') as jsonfile:
+        jsonfile.write(json.dumps(mydata, ensure_ascii=False, indent = 4))
+
+# filenames
+csvFilename = r'bios.csv'
+jsonFilename = r'data/bios.json'
+
+# Calling the convjson function
+convjson(csvFilename, jsonFilename)
